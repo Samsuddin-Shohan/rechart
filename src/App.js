3bd645a7e87cf86e341595d38abe6e39ed7c1c7e
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-01-06 12:26:42 +0600

    another chart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Chart1 from './Components/Chart1/Chart1';
+import Chart2 from './Components/Chart2/Chart2';
 
 
 function App() {
   return (
     <div className="App">
       <Chart1></Chart1>
+      <Chart2></Chart2>
 
     </div>
   );
